306f7ad6e740c728755b2f7d73662d437c248db0
ZhongShengping <chdzsp@163.com>
2017-04-09 12:07:22 +0800

    oslo db: check puppet resource instead of actual config in spec
    Change-Id: I92ec50ddc8a0f73bbe36f5f13c9cf8c1b7e31a63

--- spec/classes/congress_db_spec.rb
+++ spec/classes/congress_db_spec.rb
@@ -4,14 +4,16 @@ describe 'congress::db' do
 
   shared_examples 'congress::db' do
     context 'with default parameters' do
-      it { is_expected.to contain_congress_config('database/connection').with_value('sqlite:////var/lib/congress/congress.sqlite') }
-      it { is_expected.to contain_congress_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_congress_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_congress_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_congress_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_congress_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_congress_config('database/max_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_congress_config('database/max_overflow').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_oslo__db('congress_config').with(
+        :db_max_retries => '<SERVICE DEFAULT>',
+        :connection     => 'sqlite:////var/lib/congress/congress.sqlite',
+        :idle_timeout   => '<SERVICE DEFAULT>',
+        :min_pool_size  => '<SERVICE DEFAULT>',
+        :max_pool_size  => '<SERVICE DEFAULT>',
+        :max_retries    => '<SERVICE DEFAULT>',
+        :retry_interval => '<SERVICE DEFAULT>',
+        :max_overflow   => '<SERVICE DEFAULT>',
+      )}
     end
 
     context 'with specific parameters' do
@@ -27,19 +29,21 @@ describe 'congress::db' do
         }
       end
 
-      it { is_expected.to contain_congress_config('database/connection').with_value('mysql+pymysql://congress:congress@localhost/congress') }
-      it { is_expected.to contain_congress_config('database/idle_timeout').with_value('3601') }
-      it { is_expected.to contain_congress_config('database/min_pool_size').with_value('2') }
-      it { is_expected.to contain_congress_config('database/db_max_retries').with_value('-1') }
-      it { is_expected.to contain_congress_config('database/max_retries').with_value('11') }
-      it { is_expected.to contain_congress_config('database/retry_interval').with_value('11') }
-      it { is_expected.to contain_congress_config('database/max_pool_size').with_value('11') }
-      it { is_expected.to contain_congress_config('database/max_overflow').with_value('21') }
+      it { is_expected.to contain_oslo__db('congress_config').with(
+        :db_max_retries => '-1',
+        :connection     => 'mysql+pymysql://congress:congress@localhost/congress',
+        :idle_timeout   => '3601',
+        :min_pool_size  => '2',
+        :max_pool_size  => '11',
+        :max_retries    => '11',
+        :retry_interval => '11',
+        :max_overflow   => '21',
+      )}
     end
 
     context 'with postgresql backend' do
       let :params do
-        { :database_connection     => 'postgresql://congress:congress@localhost/congress', }
+        { :database_connection => 'postgresql://congress:congress@localhost/congress', }
       end
 
       it 'install the proper backend package' do
@@ -50,7 +54,7 @@ describe 'congress::db' do
 
     context 'with MySQL-python library as backend package' do
       let :params do
-        { :database_connection     => 'mysql://congress:congress@localhost/congress', }
+        { :database_connection => 'mysql://congress:congress@localhost/congress', }
       end
 
       it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
@@ -58,7 +62,7 @@ describe 'congress::db' do
 
     context 'with incorrect database_connection string' do
       let :params do
-        { :database_connection     => 'foodb://congress:congress@localhost/congress', }
+        { :database_connection => 'foodb://congress:congress@localhost/congress', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -66,7 +70,7 @@ describe 'congress::db' do
 
     context 'with incorrect pymysql database_connection string' do
       let :params do
-        { :database_connection     => 'foo+pymysql://congress:congress@localhost/congress', }
+        { :database_connection => 'foo+pymysql://congress:congress@localhost/congress', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -77,7 +81,7 @@ describe 'congress::db' do
   shared_examples_for 'congress::db on Debian' do
     context 'using pymysql driver' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://congress:congress@localhost/congress', }
+        { :database_connection => 'mysql+pymysql://congress:congress@localhost/congress', }
       end
 
       it 'install the proper backend package' do
@@ -93,7 +97,7 @@ describe 'congress::db' do
   shared_examples_for 'congress::db on RedHat' do
     context 'using pymysql driver' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://congress:congress@localhost/congress', }
+        { :database_connection => 'mysql+pymysql://congress:congress@localhost/congress', }
       end
 
       it 'install the proper backend package' do
