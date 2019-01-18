b19ff77960e514f7a72fe19645a6b2c7a445489d
Tobias Urdin <tobias.urdin@binero.se>
2019-01-18 10:14:17 +0100

    Remove redundantly tested code
    Remove code that is redundantly tested.
    
    This should not be tested here but in puppet-oslo
    where this logic resides.
    
    If we keep this and we do changes in puppet-oslo we
    will break these unit tests, this is something we need
    to sort out for all modules.
    
    Change-Id: If3c6145f65a2d2fa63a3f9d38078b4bca021f811

--- spec/classes/congress_db_spec.rb
+++ spec/classes/congress_db_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'congress::db' do
-
   shared_examples 'congress::db' do
     context 'with default parameters' do
-      it { is_expected.to contain_oslo__db('congress_config').with(
+      it { should contain_class('congress::deps') }
+
+      it { should contain_oslo__db('congress_config').with(
         :db_max_retries => '<SERVICE DEFAULT>',
         :connection     => 'sqlite:////var/lib/congress/congress.sqlite',
         :idle_timeout   => '<SERVICE DEFAULT>',
@@ -19,7 +20,8 @@ describe 'congress::db' do
 
     context 'with specific parameters' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://congress:congress@localhost/congress',
+        {
+          :database_connection     => 'mysql+pymysql://congress:congress@localhost/congress',
           :database_idle_timeout   => '3601',
           :database_min_pool_size  => '2',
           :database_db_max_retries => '-1',
@@ -31,7 +33,9 @@ describe 'congress::db' do
         }
       end
 
-      it { is_expected.to contain_oslo__db('congress_config').with(
+      it { should contain_class('congress::deps') }
+
+      it { should contain_oslo__db('congress_config').with(
         :db_max_retries => '-1',
         :connection     => 'mysql+pymysql://congress:congress@localhost/congress',
         :idle_timeout   => '3601',
@@ -43,79 +47,17 @@ describe 'congress::db' do
         :pool_timeout   => '21',
       )}
     end
-
-    context 'with postgresql backend' do
-      let :params do
-        { :database_connection => 'postgresql://congress:congress@localhost/congress', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-psycopg2').with(:ensure => 'present')
-      end
-
-    end
-
-    context 'with MySQL-python library as backend package' do
-      let :params do
-        { :database_connection => 'mysql://congress:congress@localhost/congress', }
-      end
-
-      it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
-    end
-
-    context 'with incorrect database_connection string' do
-      let :params do
-        { :database_connection => 'foodb://congress:congress@localhost/congress', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-    context 'with incorrect pymysql database_connection string' do
-      let :params do
-        { :database_connection => 'foo+pymysql://congress:congress@localhost/congress', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-  end
-
-  shared_examples_for 'congress::db on Debian' do
-    context 'using pymysql driver' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://congress:congress@localhost/congress', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-pymysql').with(
-          :ensure => 'present',
-          :name   => 'python-pymysql',
-          :tag    => 'openstack'
-        )
-      end
-    end
-  end
-
-  shared_examples_for 'congress::db on RedHat' do
-    context 'using pymysql driver' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://congress:congress@localhost/congress', }
-      end
-
-    end
   end
 
   on_supported_os({
-    :supported_os   => OSDefaults.get_supported_os
+    :supported_os => OSDefaults.get_supported_os
   }).each do |os,facts|
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'congress::db'
-      it_configures "congress::db on #{facts[:osfamily]}"
+      it_behaves_like 'congress::db'
     end
   end
 end
