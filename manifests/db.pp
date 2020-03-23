af0b924955050a2bffea5d0fa1efd3d59a515e0e
ZhongShengping <chdzsp@163.com>
2020-03-23 14:45:48 +0800

    Remove idle_timeout option
    The idle_timeout parameter has been deprecated for two releases.
    We can remove it.
    
    Change-Id: I387bee44e53fb7bcfe2d42c2c48c4e319ac51ad3

--- manifests/db.pp
+++ manifests/db.pp
@@ -42,12 +42,6 @@
 #   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
 #   Defaults to $::os_service_default
 #
-# DEPRECATED PARAMETERS
-#
-# [*database_idle_timeout*]
-#   Timeout when db connections should be reaped.
-#   Defaults to undef.
-#
 class congress::db (
   $database_connection              = 'sqlite:////var/lib/congress/congress.sqlite',
   $database_connection_recycle_time = $::os_service_default,
@@ -58,8 +52,6 @@ class congress::db (
   $database_retry_interval          = $::os_service_default,
   $database_max_overflow            = $::os_service_default,
   $database_pool_timeout            = $::os_service_default,
-  # DEPRECATED PARAMETERS
-  $database_idle_timeout            = undef,
 ) {
 
   include congress::deps
@@ -67,15 +59,9 @@ class congress::db (
   validate_legacy(Oslo::Dbconn, 'validate_re', $database_connection,
     ['^(sqlite|mysql(\+pymysql)?|postgresql):\/\/(\S+:\S+@\S+\/\S+)?'])
 
-  if $database_idle_timeout {
-    warning('The database_idle_timeout parameter is deprecated. Please use \
-database_connection_recycle_time instead.')
-  }
-  $database_connection_recycle_time_real = pick($database_idle_timeout, $database_connection_recycle_time)
-
   oslo::db { 'congress_config':
     connection              => $database_connection,
-    connection_recycle_time => $database_connection_recycle_time_real,
+    connection_recycle_time => $database_connection_recycle_time,
     min_pool_size           => $database_min_pool_size,
     db_max_retries          => $database_db_max_retries,
     max_retries             => $database_max_retries,
